53382a3a56627a9814ee5b9839f3a8d8561935c1
blognn_287 <83860336+pikadragonelse@users.noreply.github.com>
2023-06-01 09:13:55 +0700

    fix: link home

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
-import { DetailPage } from './page/detail-page';
+import { Routes, Route } from "react-router-dom";
+import { DetailPage } from "./page/detail-page";
 
-import { Home } from './page/home';
-import { NotFound } from './page/not-found';
-import { PageList } from './page/page-list';
+import { Home } from "./page/home";
+import { NotFound } from "./page/not-found";
+import { PageList } from "./page/page-list";
 
 export const App = () => {
     return (
         <>
             <Routes>
-                <Route path="/" element={<Home />}></Route>
+                <Route path="/Nura" element={<Home />}></Route>
                 <Route path="/page-list" element={<PageList />} />
-                <Route path="/detail-page/:productId" element={<DetailPage />} />
+                <Route
+                    path="/detail-page/:productId"
+                    element={<DetailPage />}
+                />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </>
