c3c424de471b475a407c7d3060da397f6a25f349
blognn_287 <83860336+pikadragonelse@users.noreply.github.com>
2023-06-01 09:31:39 +0700

    hello

--- src/page/not-found/not-found.tsx
+++ src/page/not-found/not-found.tsx
@@ -1,23 +1,24 @@
-import classNames from 'classnames/bind';
+import classNames from "classnames/bind";
 
-import { Button } from '../../component/button';
-import { Wrapper } from '../../component/wrapper';
+import { Button } from "../../component/button";
+import { Wrapper } from "../../component/wrapper";
 
-import styles from './not-found.module.scss';
+import styles from "./not-found.module.scss";
 
 const cl = classNames.bind(styles);
 
 export const NotFound = () => {
     return (
-        <Wrapper type="wrapperPage" className={cl('notFoundWrapper')}>
-            <h1 className={cl('authorName')}>Dragon Pi</h1>
-            <h1 className={cl('errorTitle', 'errorItem')}>404</h1>
-            <h3 className={cl('errorContent', 'errorItem')}>Page not found</h3>
-            <p className={cl('errorDesc', 'errorItem')}>
-                You can either return to the previous page or visit our homepage.
+        <Wrapper type="wrapperPage" className={cl("notFoundWrapper")}>
+            <h1 className={cl("authorName")}>Dragon Pi</h1>
+            <h1 className={cl("errorTitle", "errorItem")}>404</h1>
+            <h3 className={cl("errorContent", "errorItem")}>Page not found</h3>
+            <p className={cl("errorDesc", "errorItem")}>
+                You can either return to the previous page or visit our
+                homepage.
             </p>
 
-            <Button className={cl('notFoundButton')} to="/">
+            <Button className={cl("notFoundButton")} to="/Nura/">
                 Back to home
             </Button>
         </Wrapper>
