cd53a9752004e5786059a0f82059916709dfa5ef
blognn_287 <83860336+pikadragonelse@users.noreply.github.com>
2023-06-01 09:24:11 +0700

    fix: url

--- src/component/sidebar/sidebar.tsx
+++ src/component/sidebar/sidebar.tsx
@@ -1,27 +1,44 @@
-import classNames from 'classnames/bind';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleQuestion, faHeadphones, faHouse, faUsersViewfinder } from '@fortawesome/free-solid-svg-icons';
+import classNames from "classnames/bind";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faCircleQuestion,
+    faHeadphones,
+    faHouse,
+    faUsersViewfinder,
+} from "@fortawesome/free-solid-svg-icons";
 
-import { SidebarListItem } from './sidebar-list-item';
-import styles from './sidebar.module.scss';
+import { SidebarListItem } from "./sidebar-list-item";
+import styles from "./sidebar.module.scss";
 
 const cl = classNames.bind(styles);
 
 export const Sidebar = () => {
     return (
-        <div className={cl('sidebar', 'sidebarHome')}>
-            <ul className={cl('sidebarListContent')}>
-                <SidebarListItem to="/">
-                    <FontAwesomeIcon className={cl('iconSidebar')} icon={faHouse} />
+        <div className={cl("sidebar", "sidebarHome")}>
+            <ul className={cl("sidebarListContent")}>
+                <SidebarListItem to="/Nura/">
+                    <FontAwesomeIcon
+                        className={cl("iconSidebar")}
+                        icon={faHouse}
+                    />
                 </SidebarListItem>
                 <SidebarListItem to="/page-list">
-                    <FontAwesomeIcon className={cl('iconSidebar')} icon={faHeadphones} />
+                    <FontAwesomeIcon
+                        className={cl("iconSidebar")}
+                        icon={faHeadphones}
+                    />
                 </SidebarListItem>
-                <SidebarListItem to="/for-you" className={cl('')}>
-                    <FontAwesomeIcon className={cl('iconSidebar')} icon={faUsersViewfinder} />
+                <SidebarListItem to="/for-you" className={cl("")}>
+                    <FontAwesomeIcon
+                        className={cl("iconSidebar")}
+                        icon={faUsersViewfinder}
+                    />
                 </SidebarListItem>
-                <SidebarListItem to="/about-us" className={cl('')}>
-                    <FontAwesomeIcon className={cl('iconSidebar')} icon={faCircleQuestion} />
+                <SidebarListItem to="/about-us" className={cl("")}>
+                    <FontAwesomeIcon
+                        className={cl("iconSidebar")}
+                        icon={faCircleQuestion}
+                    />
                 </SidebarListItem>
             </ul>
         </div>
