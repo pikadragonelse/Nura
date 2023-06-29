9ff5f7baf04a2210b9ebf80fb107a7cffae1d1dd
blognn_287 <83860336+pikadragonelse@users.noreply.github.com>
2023-06-29 16:18:32 +0700

    fix: route

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <Router>
+            <Router basename="/Nura">
                 <App />
             </Router>
         </Provider>
